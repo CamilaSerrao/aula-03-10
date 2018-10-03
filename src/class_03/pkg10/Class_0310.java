cabf9408843f122d52aec28588383a18209fe42b
Aluno <Aluno@10.0.90.231>
2018-10-03 12:24:34 -0400

    1st commit

---
+++ src/class_03/pkg10/Class_0310.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class_03.pkg10;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Class_0310 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("today is 03/10");
+    }
+    
+}
